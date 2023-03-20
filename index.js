4a89eda16dbb7df16eaa10e050e18451fdec65dd
Eli Cadena <103008210+ecadena7@users.noreply.github.com>
2023-03-19 22:21:39 -0600

    created index.js file - added TODO items.

---
+++ index.js
@@ -0,0 +1,18 @@
+const inquirer = require("inquirer");
+const fs = require('fs')
+
+
+// TODO writeToFile this will use the 'fs' found in line 2
+
+
+// TODO generate an HTML ← function
+
+// TODO function to add an intern
+
+// TODO function to add an engineer
+
+// TODO function to add an employee(non-mgmt)
+
+// TODO function to add an employee(mgmt)
+
+// TODO function to call init();
