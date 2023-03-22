8fcb279dac44fbdea25d3271c27390e40e52c716
Eli Cadena <103008210+ecadena7@users.noreply.github.com>
2023-03-21 18:27:01 -0600

    created generateHTML.js file w/ TODO items

---
+++ src/generateHTML.js
@@ -0,0 +1,35 @@
+
+
+function renderHTML(employeeArray) {
+//create a for loop for the employee array
+//create a card for each employee in the array
+//add the cards in my html code
+
+    return `
+    <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- link tailwind css -->
+    <title>Special Team Generator</title>
+</head>
+
+<body>
+    <header>
+        <p>My Special Journey Team</p>
+    </header>
+
+    <!-- create an employee card -->
+    
+</body>
+</html>
+    `
+
+
+
+}
+
+module.exports = renderHTML;
+
